a8bcaf74ad00525646126f3840fe35ca259ecdf5
RD <racheal.larimer@gmail.com>
2024-01-07 16:14:38 -0800

    add comments in case I ever get confused trying to work on this monster of a component again

--- sanity/schemas/components/ExercisePreviewComponent.jsx
+++ sanity/schemas/components/ExercisePreviewComponent.jsx
@@ -8,18 +8,22 @@ export const ExercisePreviewComponent = props => {
   const { superset, renderDefault, _key: key } = props;
   const [open, setOpen] = useState(false);
   const [isSupersetMember, setIsSupersetMember] = useState(superset);
-  const [groupColor, setGroupColor] = useState(null);
+  const [groupColorIndex, setGroupColorIndex] = useState(null);
   const parent = useFormValue(['exercises']);
 
   const groupBySuperset = array => {
     if (!array.length) return [];
     return array.reduce((groups, item, index) => {
+      //If the current item is a superset and the previous item is not, start a new group
       if (item?.superset && !array[index - 1]?.superset) {
         groups.push([item]);
+        //If the current item is a superset or the previous item is a superset, add it to a group
       } else if (item?.superset || array[index - 1]?.superset) {
-        if (groups.length === 0 || !groups[groups.length - 1][0]?.superset) {
+        //Create a new group if there are no groups yet
+        if (groups.length === 0) {
           groups.push([item]);
         } else {
+          //Add the item to the last group if there are groups
           groups[groups.length - 1].push(item);
         }
       }
@@ -31,23 +35,27 @@ export const ExercisePreviewComponent = props => {
     if (superset) {
       setIsSupersetMember(superset);
     }
+
     if (parent) {
       if (!superset) {
+        //If the current exercise is not a superset, check if the previous exercise is
         const prevIndex = parent.findIndex(({ _key }) => _key === key) - 1;
 
         setIsSupersetMember(parent?.[prevIndex]?.superset);
       }
 
+      //Get all the superset groups
       const groups = groupBySuperset(parent).map(group =>
         group.map(({ _key }) => _key)
       );
-
+      //Find the index of the current exercise's superset group
       const groupIndex = groups.findIndex(group => group.includes(key));
 
+      //Set the group color based on the index
       if (groupIndex < 2) {
-        setGroupColor(groupIndex);
+        setGroupColorIndex(groupIndex);
       } else {
-        setGroupColor(groupIndex % 2);
+        setGroupColorIndex(groupIndex % 2);
       }
     }
   }, [parent, superset, key]);
@@ -68,7 +76,7 @@ export const ExercisePreviewComponent = props => {
         >
           <div onMouseEnter={handleMouseEvent} onMouseLeave={handleMouseEvent}>
             <LinkIcon
-              style={{ fontSize: 24, color: supersetColors[groupColor] }}
+              style={{ fontSize: 24, color: supersetColors[groupColorIndex] }}
             />
           </div>
         </Popover>
